bf5952e74e84d0080351cd6256571609d043e96f
FeliciousX <feliciousx@gmail.com>
2018-07-24 23:34:36 +0800

    feat(user.service): craft API

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,24 +2,50 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, pluck } from 'rxjs/operators';
+import uuidv4 from 'uuidv4';
 
 import { User } from './user';
-import { USERS } from './mock-users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private userUrl = 'https://randomuser.me/api/';
+  private userUrl = 'https://randomuser.me/api';
+  private params: Record<string, string> = {
+    exc: 'login', // exclude login always (because it's heavy)
+    seed: uuidv4(),
+    results: '10', // take 10 per page
+    page: '0'
+  };
 
   constructor(private http: HttpClient) {}
 
+  get apiUrl(): string {
+    return this.buildUrl(this.params, this.userUrl);
+  }
+
+  /**
+   * Return list of users from API
+   *
+   * NOTE: function contains side effect that increments params.page
+   */
   getUsers(): Observable<User[]> {
-    return this.http.get<{results: User[]}>(this.userUrl)
+    this.incrementPage();
+    return this.http.get<{results: User[]}>(this.apiUrl)
     .pipe(
       pluck<{results: User[]}, User[]>('results'),
       catchError(this.handleError<User[]>('getUsers', []))
     );
+  }
+
+  incrementPage() {
+    this.params.page = (Number.parseInt(this.params.page) + 1).toString();
+  }
+
+  private buildUrl(params: Record<string, string>, url: string): string {
+    return Object.keys(params).reduce((paramStr, key) => {
+      return paramStr.concat( `${key}=${params[key]}&` );
+    }, `${url}/?`);
   }
 
   /**
@@ -30,7 +56,6 @@ export class UserService {
     return (error: any): Observable<T> => {
       console.error(error);
 
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
