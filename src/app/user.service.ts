466deea5aefd2f42833810c3f41ec0b6c2fff819
FeliciousX <feliciousx@gmail.com>
2018-07-24 22:48:07 +0800

    feat(service): user service to use http request to API

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, pluck } from 'rxjs/operators';
 
 import { User } from './user';
 import { USERS } from './mock-users';
@@ -8,10 +10,28 @@ import { USERS } from './mock-users';
   providedIn: 'root'
 })
 export class UserService {
+  private userUrl = 'https://randomuser.me/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    return of(USERS);
+    return this.http.get<{results: User[]}>(this.userUrl)
+    .pipe(
+      pluck<{results: User[]}, User[]>('results'),
+      catchError(this.handleError<User[]>('getUsers', []))
+    );
+  }
+
+  /**
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
